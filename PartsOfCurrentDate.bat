7876726a842a27e70149fb178ed5706b9a4e7862
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-03-23 20:36:26 +0530

    Add files via upload

---
+++ PartsOfCurrentDate.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Current date : %date%
+echo Year of Current date : %date:~10,4%
+echo Date of Current date : %date:~7,2%
+echo Month of Current date : %date:~4,2%
+echo Weekday of Current date : %date:~0,3%
+
+pause
