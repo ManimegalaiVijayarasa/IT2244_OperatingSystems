9516a6d173932544aceb8663e0381fd57f2c410e
ManimegalaiVijayarasa <megala0902@gmail.com>
2025-03-23 20:32:16 +0530

    Add files via upload

---
+++ createFolders.bat
@@ -0,0 +1,23 @@
+mkdir Criteria_1
+cd Criteria_1
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+cd..
+mkdir Criteria_2
+cd Criteria_2
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+cd..
+mkdir Criteria_3
+cd Criteria_3
+mkdir Standard_1
+mkdir Standard_2
+mkdir Standard_3
+mkdir Standard_4
+cd..
+mkdir Criteria_4
+mkdir Criteria_5
+
